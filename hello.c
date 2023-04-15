c13b6f6021bad4ebb5b6303ae507582dad1a16ad
JungMin-E <dlwjdals273@naver.com>
2023-04-15 22:38:35 +0900

    최초 commit 메세지

---
+++ hello.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+//2진수를 10진수로 바꾸기
+
+int main()
+{
+	int decimal = 0;
+	int binary[4] = {1, 1, 0, 1};
+	int pos = 0;
+
+	for(int i =(sizeof(binary) / sizeof(int)) - 1; i >=0; i--)
+	{
+		if(binary[i] == 1)
+			decimal += 1 << pos; //decimal = decimal + 1 << po
+		
+		pos++;
+	}
+	printf("%d\n", decimal);
+
+	return 0;
+}
